c223f85e9057ba1a1c669cf60083a2502ab3d28f
Dell 7710 <ariad.mr95@gmail.com>
2024-05-01 00:30:56 +0300

    remove unused dialogs

--- app/src/main/java/com/doubleclick/rovleapp/feature/auth/forgetPassword/presentations/ForgetPasswordStepThreeFragment.kt
+++ app/src/main/java/com/doubleclick/rovleapp/feature/auth/forgetPassword/presentations/ForgetPasswordStepThreeFragment.kt
@@ -1,10 +1,7 @@
 package com.doubleclick.restaurant.feature.auth.forgetPassword.presentations
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
@@ -17,7 +14,6 @@ import com.doubleclick.restaurant.core.functional.Either
 import com.doubleclick.restaurant.core.functional.ProgressHandler
 import com.doubleclick.restaurant.core.platform.BaseFragment
 import com.doubleclick.restaurant.databinding.FragmentForgetPasswordStepThreeBinding
-import com.doubleclick.restaurant.dialog.DialogForgetPassword
 import com.doubleclick.restaurant.feature.auth.AuthViewModel
 import com.doubleclick.restaurant.feature.auth.forgetPassword.data.ForgetPasswordResponse
 import dagger.hilt.android.AndroidEntryPoint
@@ -74,16 +70,16 @@ class ForgetPasswordStepThreeFragment : BaseFragment(R.layout.fragment_forget_pa
     }
 
     private fun renderResetPassword(@Suppress("UNUSED_PARAMETER")data: ForgetPasswordResponse) {
-        val dialog = DialogForgetPassword(requireActivity())
-        dialog.show()
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        dialog.setCancelable(false)
-        dialog.setCanceledOnTouchOutside(false)
-        dialog.findViewById<TextView>(R.id.terminar)?.setOnClickListener {
-            dialog.dismiss()
-            viewModel.isBackPressedDispatcherEnabled(false)
-            requireActivity().onBackPressedDispatcher.onBackPressed()
-        }
+//        val dialog = DialogForgetPassword(requireActivity())
+//        dialog.show()
+//        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//        dialog.setCancelable(false)
+//        dialog.setCanceledOnTouchOutside(false)
+//        dialog.findViewById<TextView>(R.id.terminar)?.setOnClickListener {
+//            dialog.dismiss()
+//            viewModel.isBackPressedDispatcherEnabled(false)
+//            requireActivity().onBackPressedDispatcher.onBackPressed()
+//        }
     }
 
     private fun renderLoading(loading: Either.Loading) {
